cbc88307e71e536f558ae4929b790514e49ff94c
Abid Hasan <developer.abid@gmail.com>
2019-08-24 14:46:42 +0600

    added map view to map fragments

--- app/src/main/java/com/example/runner/feature_module/fragments/MapFragment.kt
+++ app/src/main/java/com/example/runner/feature_module/fragments/MapFragment.kt
@@ -8,12 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.runner.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.custom_topbar.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    lateinit var mMap:GoogleMap
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +36,20 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tvActionBarTitle.text = getText(R.string.map_text)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+
+        if (googleMap != null) {
+            mMap = googleMap
+        }
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16F))
     }
 }
